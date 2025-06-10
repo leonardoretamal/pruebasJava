b32cf72c70a707218a099c9057b79e3e07f49d00
Leonardo Retamal <leonardo.retamal.morales@gmail.com>
2025-06-10 13:43:41 -0400

    mi primer commit

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,26 @@
+package holamundo;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+       
+        //operacion de asignacion
+        int edad = 15;
+        double estatura = 1.79 ;
+        boolean tieneCarnet = true;
+        char letra = 'L';
+        String direccion = "Direccion 123";
+        long numeroLargo = 312455423;
+        
+        System.out.println("Hola la edad es: "+edad);
+        System.out.println("La estatura es: "+ estatura);
+        System.out.println("tiene carnet: "+tieneCarnet);
+        System.out.println("Letra favorita: "+letra );
+        System.out.println("La direcccion es: "+direccion);
+        System.out.println("El numero largo es: "+numeroLargo);
+        
+        
+        
+    }
+
+}
